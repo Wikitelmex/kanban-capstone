bf17835024e337632f97eafce7ee715fa4806922
Alex Castillo <wikitelmex@gmail.com>
2021-10-12 09:49:38 -0600

    linters fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './style.css';
-import { myLocalStorage } from "./modules/myLocalstorage";
-
-function component() {
-  const element = document.createElement('div');
-  const myLS = new myLocalStorage;
-
-  myLS.save();
-
-  
-
-  return element;
-}
-
-document.body.appendChild(component());
