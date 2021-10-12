d97edb3d04ed4097ee177bc672ab0bc0c0646fcd
Alex Castillo <wikitelmex@gmail.com>
2021-10-12 13:12:19 -0600

    Characters-list
    - add http request class
    - add dom manipulator
    - add card template inside a class
    - add dynamic list of cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import { MyHttpRequest } from './modules/httpRequests.js';
+import { DomRequest } from './modules/domRequests.js';
+import { Templates } from './modules/domTemplates.js';
+
+const httprequester = new MyHttpRequest('https://www.breakingbadapi.com/api/characters');
+let myArray = [];
+
+window.refreshData = () => {
+  DomRequest.clear('cardsContainer');
+  httprequester.getAsync().then((res) => {
+    myArray = res;
+    myArray.forEach((element) => {
+      DomRequest.appendTemplate('cardsContainer', Templates.CharacterCard(element));
+    });
+  });
+};
+
+window.onload = () => {
+  window.refreshData();
+};
