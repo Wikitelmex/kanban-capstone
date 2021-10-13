e14eef84ba63243ad556cc6796cbaf201124e8a7
Alex Castillo <wikitelmex@gmail.com>
2021-10-13 10:35:30 -0600

    Testing
    - add testing to element counter

--- src/tests/tools.test.js
+++ src/tests/tools.test.js
@@ -9,6 +9,20 @@ const mockArray = [
     {id: 0}
 ];
 
+const mockArray1 = [1,2,3]
+
 test('elements counter should return 6', () => {
     expect(elementsCounter(mockArray)).toBe(6);
-  });
+});
+
+test('elements counter should return 6', () => {
+    expect(elementsCounter(mockArray)).not.toBe(3);
+});
+
+test('elements counter should return 3', () => {
+    expect(elementsCounter(mockArray1)).toBe(3);
+});
+
+test('elements counter should return 3', () => {
+    expect(elementsCounter(mockArray1)).not.toBe(6);
+});
